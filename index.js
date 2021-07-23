6bfa2acc2559831c1c57f9e3f24c8d16b7649688
udibkl <68611381+udibkl@users.noreply.github.com>
2021-07-24 01:30:16 +0530

    Add files via upload
    version 1

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    let dat= new Date;
+    elem=document.getElementById('text');
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+   let date = dat.toLocaleDateString(undefined, options);
+    let time=dat.getHours()+":"+dat.getMinutes()+":"+dat.getSeconds();
+elem.innerHTML="<h1>Date and Time is <br></h1>"+date+"<br>"+time;
+
+
+}, 1000);
+
+
